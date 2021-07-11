89434c8196d5bf11fca9d8bafa9f8052200cb64b
Viktoriia Anfilofeva <anfilofevavs@gmail.com>
2021-07-11 15:33:19 +0200

    add homework_1 Git Bash

---
+++ homework_1/skript_1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir autoskript
+ls -la
+mkdir f_1 f_2 f_3
+ls -la
+cd f_1
+ls -la
+touch test1.txt test2.txt test3.txt test4.json test5.json
+ls -la
+mkdir f_4 f_5 f_6
+ls -la
+mv test4.json test5.json f_5
+ls -la
+
